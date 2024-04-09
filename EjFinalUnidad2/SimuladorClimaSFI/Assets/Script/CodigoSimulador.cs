76b4bff8466b70394bfda40acbe5f12cd31d8cf6
andruAJ <andres.giraldov@upb.edu.co>
2024-04-09 13:58:04 -0500

    codigo unity cambios

--- EjFinalUnidad2/SimuladorClimaSFI/Assets/Script/CodigoSimulador.cs
+++ EjFinalUnidad2/SimuladorClimaSFI/Assets/Script/CodigoSimulador.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 enum TaskState
@@ -18,6 +19,7 @@ public class CodigoSimulador : MonoBehaviour
     [SerializeField] private Vector2 velocidadMovimiento;
     private Vector2 offset;
     private Material material;
+    public TextMeshProUGUI myText;
 
     void Start()
     {
@@ -41,23 +43,88 @@ public class CodigoSimulador : MonoBehaviour
             case TaskState.COMMANDS:
                 if (Input.GetKeyDown(KeyCode.A))
                 {
-                    _serialPort.Write("ledON\n");
+                    _serialPort.Write("rainUp\n");
                     Debug.Log("Send ledON");
                 }
                 if (Input.GetKeyDown(KeyCode.S))
                 {
-                    _serialPort.Write("ledOFF\n");
+                    _serialPort.Write("rainDown\n");
                     Debug.Log("Send ledOFF");
                 }
-                if (Input.GetKeyDown(KeyCode.R))
+                if (Input.GetKeyDown(KeyCode.D))
                 {
-                    _serialPort.Write("readBUTTONS\n");
+                    _serialPort.Write("timeUp\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    _serialPort.Write("timeDown\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    _serialPort.Write("tempUp\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    _serialPort.Write("tempDown\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.J))
+                {
+                    _serialPort.Write("tempSpeedUp\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.K))
+                {
+                    _serialPort.Write("tempSpeedDown\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.L))
+                {
+                    _serialPort.Write("rainSpeedUp\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    _serialPort.Write("rainSpeedDown\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+                    _serialPort.Write("tempUp\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    _serialPort.Write("tempDown\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.V))
+                {
+                    _serialPort.Write("rainUp\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    _serialPort.Write("rainDown\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.N))
+                {
+                    _serialPort.Write("out\n");
+                    Debug.Log("Send readBUTTONS");
+                }
+                if (Input.GetKeyDown(KeyCode.M))
+                {
+                    _serialPort.Write("in\n");
                     Debug.Log("Send readBUTTONS");
                 }
                 if (_serialPort.BytesToRead > 0)
                 {
                     string response = _serialPort.ReadLine();
-                    Debug.Log(response);
+                    myText.text = response;
                 }
                 break;
             default:
