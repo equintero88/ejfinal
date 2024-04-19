8849f7d179efbfb09c2c2fbd4d6480a8515b02af
andruAJ <andres.giraldov@upb.edu.co>
2024-04-18 22:31:18 -0500

    cambios al codigo de unity y algunos a arduino

--- EjFinalUnidad2/SimuladorClimaSFI/Assets/Script/CodigoSimulador.cs
+++ EjFinalUnidad2/SimuladorClimaSFI/Assets/Script/CodigoSimulador.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine;
+using System;
 
 enum TaskState
 {
@@ -35,6 +36,14 @@ enum TaskStateTemperaturaBajar
     COMMANDS
 }
 
+enum StateEscenas
+{
+    INIT,
+    NOLLUVIA,
+    LLOVIZNA,
+    LLUVIACONSTANTE,
+    AGUACERO
+}
 public class CodigoSimulador : MonoBehaviour
 {
     private static TaskState taskState = TaskState.INIT;
@@ -42,11 +51,10 @@ public class CodigoSimulador : MonoBehaviour
     private static TaskStateLluviaBajar taskState2 = TaskStateLluviaBajar.INIT;
     private static TaskStateTemperaturaSubir taskState3 = TaskStateTemperaturaSubir.INIT;
     private static TaskStateTemperaturaBajar taskState4 = TaskStateTemperaturaBajar.INIT;
+    private static StateEscenas stateEscenas = StateEscenas.INIT;
     private SerialPort _serialPort = new SerialPort();
     private byte[] buffer = new byte[32];
     [SerializeField] private Vector2 velocidadMovimiento;
-    private Vector2 offset;
-    private Material material;
     public TextMeshProUGUI myText;
     public Scene escena;
 
@@ -57,7 +65,6 @@ public class CodigoSimulador : MonoBehaviour
         _serialPort.DtrEnable = true;
         _serialPort.Open();
         Debug.Log("Open Serial Port");
-        material = GetComponent<SpriteRenderer>().material;
     }
 
     
@@ -398,13 +405,221 @@ public class CodigoSimulador : MonoBehaviour
     }
     public void IniciarEscena()
     {
+        switch (stateEscenas)
+        {
+            case StateEscenas.INIT:
+                _serialPort.Write("estadoLluvia\n");
+                string respuesta = _serialPort.ReadLine();
+                if (respuesta == "0") {
+                    stateEscenas = StateEscenas.NOLLUVIA;
+                    Debug.Log("NOLLUVIA");
+                }
+                if (respuesta == "1")
+                {
+                    stateEscenas = StateEscenas.LLOVIZNA;
+                    Debug.Log("LLOVIZNA");
+                }
+                if (respuesta == "2")
+                {
+                    stateEscenas = StateEscenas.LLUVIACONSTANTE;
+                    Debug.Log("LLUVIACONSTANTE");
+                }
+                if (respuesta == "3")
+                {
+                    stateEscenas = StateEscenas.AGUACERO;
+                    Debug.Log("AGUACERO");
+                }
+
+                break;
+            case StateEscenas.NOLLUVIA:
+                _serialPort.Write("estadoTemp\n");
+                string resp = _serialPort.ReadLine();
+                 int temperatura = Int32.Parse(resp);
+                if (temperatura >= 14 && temperatura <= 20)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >=  6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("NoLluviaFrio");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("NoLluviaFrio");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura > 20 && temperatura <= 26)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("NoLluviaTemplado");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("NoLluviaTemplado");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura > 26 && temperatura <= 32)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("NoLluviaCaluroso");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("NoLluviaCaluroso");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                break;
+            case StateEscenas.LLOVIZNA:
+                _serialPort.Write("estadoTemp\n");
+                string resp1 = _serialPort.ReadLine();
+                int temperatura1 = Int32.Parse(resp1);
+                if (temperatura1 >= 14 && temperatura1 <= 20)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LloviznaFrio");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LloviznaFrio");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura1 > 20 && temperatura1 <= 26)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LloviznaTemplado");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LloviznaTemplado");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura1 > 26 && temperatura1 <= 32)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LloviznaCaluroso");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LloviznaCaluroso");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                break;
+            case StateEscenas.LLUVIACONSTANTE:
+                _serialPort.Write("estadoTemp\n");
+                string resp2 = _serialPort.ReadLine();
+                int temperatura2 = Int32.Parse(resp2);
+                if (temperatura2 >= 14 && temperatura2 <= 20)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LluviaConstanteFrio");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LluviaConstanteFrio");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura2 > 20 && temperatura2 <= 26)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LluviaConstanteTemplado");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LluviaConstanteTemplado");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura2 > 26 && temperatura2 <= 32)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LluviaConstanteCaluroso");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LluviaConstanteCaluroso");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                break;
+            case StateEscenas.AGUACERO:
+                _serialPort.Write("estadoTemp\n");
+                string resp3 = _serialPort.ReadLine();
+                int temperatura3 = Int32.Parse(resp3);
+                if (temperatura3 >= 14 && temperatura3 <= 20)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LluviaFuerteFrio");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LluviaFuerteFrio");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura3 > 20 && temperatura3 <= 26)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LluviaFuerteTemplado");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LluviaFuerteTemplado");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                if (temperatura3 > 26 && temperatura3 <= 32)
+                {
+                    string response = _serialPort.ReadLine();
+                    int hora = Int32.Parse(response);
+                    if (hora >= 6 && hora <= 18)
+                    {
+                        SceneManager.LoadScene("LluviaFuerteCaluroso");
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene("LluviaFuerteCaluroso");            //cambiar si hay otra escena dependiendo de la hora
+                    }
+                }
+                break;
+            default:
+                Debug.Log("State Error");
+                break;
+        }
+
         _serialPort.Write("start\n");
-        //SceneManager.LoadScene(escena.name);
+        
         if (_serialPort.BytesToRead > 0)
         {
             string response = _serialPort.ReadLine();
             myText.text = response;
         }
+
     }
 }
 
